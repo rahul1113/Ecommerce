99267c7f2b71c08513aef288ec2537e27b144417
Rahul <rahulb631@gmail.com>
2020-09-17 19:13:36 +0530

    Styling menu item and folder restructure

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DirecTory from "../../components/directory/directory";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <DirecTory />
+  </div>
+);
+
+export default HomePage;
